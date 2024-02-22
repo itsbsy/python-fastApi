50e95b31a4e2141fb176d135c6529533cf45595a
itsbsy <brijesh.yadav@w3villa.com>
2024-02-22 22:18:48 +0530

    added post route

--- index.py
+++ index.py
@@ -11,6 +11,10 @@ def read_root():
 def read_root():
     return {"Bye": "World"}
 
+@app.post("/items/")
+def create_item(item_id: int, item_name: str):
+    return {"item_id": item_id, "item_name": item_name}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, query_param: str = None):
     return {"item_id": item_id, "query_param": query_param}
